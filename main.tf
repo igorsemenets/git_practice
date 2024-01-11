44306243cc13725f7526c6cbdf5c8d651da9c4dd
Igor Semenets <igorsemenets.is@gmail.com>
2024-01-10 22:25:42 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "ubuntu_one" {
+	ami = "ami-05fb0b8c1424f266b"
+	instance_type = "t2.micro"
+	tags = {
+		Name = "good_ubuntu"
+	}
+}
