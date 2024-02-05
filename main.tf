c58abf7ddd7540de41075dd82eba8ff15766a0c9
Igor Semenets <igorsemenets.is@gmail.com>
2024-02-05 18:32:18 -0500

    configurable and working single server

--- main.tf
+++ main.tf
@@ -1,10 +1,42 @@
 provider "aws" {
   region = "us-east-2"
 }
-resource "aws_instance" "ubuntu_one" {
-	ami = "ami-05fb0b8c1424f266b"
-	instance_type = "t2.micro"
-	tags = {
-		Name = "good_ubuntu"
-	}
+resource "aws_instance" "example" {
+	ami 					= "ami-05fb0b8c1424f266b"
+	instance_type 			= "t2.micro"
+	vpc_security_group_ids 	= [aws_security_group.instance.id]
+
+
+	user_data = <<-EOF
+				#!/bin/bash
+				echo "Hello,Igorushka!" > index.html
+				nohup busybox httpd -f -p 8080 &
+				EOF
+
+user_data_replace_on_change = true
+tags = {
+	Name = "Terraform-example"
+ }
+}
+resource "aws_security_group" "instance" {
+
+	name = var.security_group_name
+
+	ingress {
+		from_port 	= 8080
+		to_port 	= 8080
+		protocol 	= "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+  
+ }
+}
+variable "security_group_name" {
+	description = "The name of the sucurity group"
+	type 		= string
+	default 	= "terraform-example-instance"
+}
+
+output "public_ip" {
+	value 	= aws_instance.example.public_ip
+	description = "The public IP of the instance"
 }
